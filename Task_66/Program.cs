222af19c254a51f158b42044a0c4078e1918819e
Said Gadjiev <ackusha1984@ya.ru>
2022-12-28 00:27:13 +0300

    Решение ДЗ 66

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -6,3 +6,19 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+
+int SummNum(int m, int n, int result = 0)
+{
+    if (m <= n) return SummNum(m+1,n, result=result+m);
+    return result;
+}
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int res = SummNum(m,n);
+
+Console.WriteLine($"M = {m}; N = {n} -> {res}");
