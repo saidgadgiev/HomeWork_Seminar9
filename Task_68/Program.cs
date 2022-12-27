1522b70cac924cb1d2114cec83c2cd425945d1ad
Said Gadjiev <ackusha1984@ya.ru>
2022-12-28 00:39:17 +0300

    Решение ДЗ 68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -6,3 +6,33 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 
 */
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Akker(m,n);
+
+
+void Akker(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
