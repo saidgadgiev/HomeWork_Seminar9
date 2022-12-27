d53874b78ee9de130ab480058ba84fb1ce444861
Said Gadjiev <ackusha1984@ya.ru>
2022-12-27 23:30:27 +0300

    Решение ДЗ 64

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -6,3 +6,15 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+
+Console.Write("Введите N: ");
+var str = Console.ReadLine() ?? "";
+
+int num = int.Parse(str);
+
+string AddN(int n)
+{
+    if (n == 1) return n.ToString();
+    else return n + ", " + AddN(n-1);
+}
+Console.WriteLine(AddN(num));
